f6bc1a70c992f144a28811b9cf88818ad6a45f77
Samuel Suh <samsuh@gmail.com>
2019-12-06 22:27:21 -0800

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY 
+    //Archon-production Mongodb
+    //mongodb+srv://samsuh:fcHCoJnCJZRjeji5@cluster0-bj1yj.mongodb.net/test?retryWrites=true&w=majority
+    //production googleClientID: '1046214905945-el05oillp0p045uaflcnunogog5sd6hi.apps.googleusercontent.com',
+    //production googleClientSecret: 'sRtlJzTLUKB2amvQLAXLauqV'
+}
